9a4230964a568ca1a21344e17471704caa2839a1
Юрчик <88848940+Yrchik07@users.noreply.github.com>
2024-04-20 19:59:08 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { createStore, combineReducers } from 'redux'
 import { createSlice } from './contactsSlice'
+import { devToolsEnhancer } from '@redux-devtools/extension';
 
 const persistReducer = combineReducers({
     contacts: createSlice
     
 })
-export const store = createStore(persistReducer)
+const enhancer = devToolsEnhancer();
+export const store = createStore(persistReducer, enhancer)
